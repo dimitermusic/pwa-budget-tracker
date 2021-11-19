5dd46e733bc5e251bdd89fc9a1e5d00a2b25e3ea
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-19 12:20:38 -0800

    fixed bugs

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
+// ROUTES
 app.use(require("./routes/api.js"));
 app.use(require("./routes/htmlRoutes.js"))
 
